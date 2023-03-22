1d92ab07f05ae44c7676daba4bf9961802fb2eb5
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-03-22 13:46:42 +0200

    add stattic pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'contact' => 'pages#contact'
+  get 'about' => 'pages#about'
+  get 'help' => 'pages#help'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
