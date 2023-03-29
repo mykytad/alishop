8ef00e07627b861be73a887f37926c936f65c557
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-03-29 18:42:20 +0300

    add new spec

--- spec/features/create_store_spec.rb
+++ spec/features/create_store_spec.rb
@@ -14,4 +14,24 @@ RSpec.feature "create store", type: :feature do
     expect(body).to have_content "Shop description"
     expect(body).to have_content "Create a product"
   end
+
+  it "creates a new store and product" do
+    log_in(test_user)
+    visit new_store_path
+    fill_in :store_name, with: "Shop"
+    fill_in :store_description, with: "Shop description"
+    fill_in :store_phone, with: "1234567890"
+    click_button "Create Store"
+
+    click_link "Create a product"
+    fill_in :product_name, with: "Phone"
+    fill_in :product_description, with: "color: space grey"
+    fill_in :product_price, with: "950"
+    fill_in :product_discount, with: "0"
+    click_button "Create Product"
+
+    expect(body).to have_content "Phone"
+    expect(body).to have_content "color: space grey"
+    expect(body).to have_content "950$"
+  end
 end
