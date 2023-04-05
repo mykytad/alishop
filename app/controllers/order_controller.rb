275818bfdfd77599eb78599a62bf77d058fb9d23
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-04-05 13:10:46 +0300

    add def creaate in order, modified checkout form

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,4 +1,10 @@
 class OrderController < ApplicationController
   def create
+    @cart = session[:cart]
+    @products = Product.where({ :id => @cart })
+    @sum = 0
+    @cart.each do |cart|
+      @sum += @products.find(cart).price
+    end
   end
 end
