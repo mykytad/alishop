d7742b70105ea9ec06754fb35b9d102568e26ff7
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-04-21 13:57:18 +0300

    modified meneger views

--- app/controllers/meneger/stores_controller.rb
+++ app/controllers/meneger/stores_controller.rb
@@ -1,5 +1,13 @@
 class Meneger::StoresController < ApplicationController
+
   def index
-    @store = Store.all
+    @stores = Store.where(:user_id => current_user.id)
+  end
+
+  def destroy
+    @store = Store.find(params[:id])
+    @store.destroy
+    
+    redirect_to meneger_stores_path
   end
 end
