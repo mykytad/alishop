fd9c74e9e635ebddd62f1f8a49ba5d0cc6460bb7
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-04-24 18:29:14 +0300

    counter new order in header

--- app/controllers/manager/orders_controller.rb
+++ app/controllers/manager/orders_controller.rb
@@ -3,6 +3,7 @@ class Manager::OrdersController < ApplicationController
     store = Store.find(params[:store_id])
     @orders = store.orders
     @orders = @orders.order(:id => :DESC)
+    # @orders_status_new = @orders.where(:status => :new)
   end
 
   def show
