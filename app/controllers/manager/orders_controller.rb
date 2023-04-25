a268d42d74cdc46fa1c1238a1769d01c7c92f10b
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-04-25 17:25:13 +0300

    modified views manager orders index/show, stores show

--- app/controllers/manager/orders_controller.rb
+++ app/controllers/manager/orders_controller.rb
@@ -1,7 +1,7 @@
 class Manager::OrdersController < ApplicationController
   def index
-    store = Store.find(params[:store_id])
-    @orders = store.orders
+    @store = Store.find(params[:store_id])
+    @orders = @store.orders
     @orders = @orders.order(:id => :DESC)
   end
 
@@ -9,6 +9,20 @@ class Manager::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_products = @order.order_products
     @products = @order.products
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    
+    if @order.update(order_params)
+      redirect_to manager_store_order_path(@order.store_id, @order.id)      
+    else
+      render :edit
+    end
   end
 
   def destroy
