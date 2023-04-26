5927f65385a3a528adef3602928e6f35fb2c7783
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-04-26 19:52:30 +0300

    fix rubocop

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,7 +2,7 @@ class Store < ApplicationRecord
   has_many :products, :dependent => :destroy
   has_many :orders
   belongs_to :user
-  
+
   validates :name, presence: true
   validates :description, presence: true
   validates :phone, presence: true, length: { minimum: 9 }
