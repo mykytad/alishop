6a1bff2793ce693f38cf319b3d2b5443c74da5f5
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-04-26 19:51:16 +0300

    fix views

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,8 +2,12 @@ class Store < ApplicationRecord
   has_many :products, :dependent => :destroy
   has_many :orders
   belongs_to :user
-
+  
   validates :name, presence: true
   validates :description, presence: true
   validates :phone, presence: true, length: { minimum: 9 }
+
+  def store_new_orders
+    orders.where(:status => :new).count
+  end
 end
