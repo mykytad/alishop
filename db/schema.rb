31cdcbc6c144ebe992f8571d12cced15b274678c
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-05-15 10:01:52 +0300

    new migration, add validations for orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_09_093717) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_15_065834) do
   create_table "order_products", force: :cascade do |t|
     t.string "product_name", null: false
     t.integer "product_price", null: false
@@ -36,7 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_09_093717) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "store_id"
-    t.integer "payment_id"
   end
 
   create_table "payments", force: :cascade do |t|
