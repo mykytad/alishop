622597dfce62374c40acdd71e1eba83b937b7a89
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-05-17 13:59:41 +0300

    redirect to manager portal after creating new store

--- app/controllers/manager/orders_controller.rb
+++ app/controllers/manager/orders_controller.rb
@@ -13,11 +13,4 @@ class Manager::OrdersController < ApplicationController
 
   def destroy
   end
-
-  def manager
-    store = Store.find(params[:store_id])
-    if current_user.id != store.user_id
-      redirect_to root_url
-    end
-  end
 end
--- app/controllers/manager/products_controller.rb
+++ app/controllers/manager/products_controller.rb
@@ -1,11 +1,4 @@
 class Manager::ProductsController < ApplicationController
   def index
   end
-
-  def manager
-    store = Store.find(params[:store_id])
-    if current_user.id != store.user_id
-      redirect_to root_url
-    end
-  end
 end
