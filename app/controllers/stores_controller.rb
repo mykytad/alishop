60e143e75dac35dac9c7d31ef2b640771d2b55dd
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-05-19 15:08:09 +0300

    remove store controller action in manager store controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -24,34 +24,9 @@ class StoresController < ApplicationController
     end
   end
 
-  def edit
-    @store = current_user_store
-  end
-
-  def update
-    @store = current_user_store
-
-    if @store.update(store_params)
-      redirect_to @store
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @store = current_user_store
-    @store.destroy
-
-    redirect_to current_user
-  end
-
   private
 
   def store_params
     params.require(:store).permit(:name, :description, :phone)
-  end
-
-  def current_user_store
-    current_user.stores.find(params[:id])
   end
 end
