cddfd0a5e3d2562cd3559e26d35cefda9d0464cd
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-05-22 14:53:25 +0300

    add review controller, model, migrations

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.review.create(review_params)
+    @review.save
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = current_user.products.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@product)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :body)  
+  end
+end
