7bd7be5f55b33f09ca987c0a89a471cf638fc413
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-05-31 10:54:14 +0300

    add images gallery for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ApplicationRecord
   has_many :reviews
 
   mount_uploader :image, ImageUploader
+  mount_uploaders :images, ImageUploader
+  serialize :images, JSON # If you use SQLite, add this line.
 
   validates :name, presence: true
   validates :description, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_22_070601) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_102033) do
   create_table "order_products", force: :cascade do |t|
     t.string "product_name", null: false
     t.integer "product_price", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_070601) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.string "images"
   end
 
   create_table "reviews", force: :cascade do |t|
