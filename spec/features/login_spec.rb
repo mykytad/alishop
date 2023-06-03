342fabaf3e1bd9ba72aa8eaa0b853d35e97e5461
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-06-03 12:07:08 +0300

    fix spec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "new sesion", type: :feature do
   it "login user" do
     log_in(test_user)
 
-    expect(body).to have_link "Profile"
+    expect(body).to have_link "Tom"
     expect(body).to have_link "Sign out"
   end
 end
