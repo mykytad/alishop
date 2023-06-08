99468fcca3bf16c0f43345f0cd3b6b809908f7c2
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-06-08 11:55:19 +0300

    create a new order without logging in and also create a new user if the user does not exist

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :load_cart
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:new, :create]
 
   def index
     @orders = Order.where(:user_id => current_user.id)
@@ -20,64 +20,37 @@ class OrdersController < ApplicationController
   end
 
   def create
-    # if :authenticate_user!
-      payments = []
-      @cart.stores.each do |store|
-        order = Order.new(order_params)
-        order.user_id = current_user.id
-        order.store_id = store.id
-        order.sum_price = 0
+    @products = @cart.products
+    @sum = @cart.sum
+    @payments = []
 
-        if order.save
-          @cart.products.where(:store_id => store.id).each do |product|
-            order_product = OrderProduct.new(
-              :product_id => product.id,
-              :order_id => order.id
-            )
-            order_product.product_price = product.price - product.discount
-            order_product.product_count = @cart.count(product.id.to_s)
-            order_product.product_name = product.name
-            order_product.product_image = product.images[0]
-            order_product.save
-          end
-          sum_price = 0
-          order.order_products.each do |product|
-            sum_price += product.product_count * product.product_price
-          end
-
-          order.sum_price = sum_price
-          if order.save
-            payment = Payment.new(
-              :order_id => order.id,
-              :user_id => order.user_id
-            )
-            payment.save
-            payments << payment
-          end
-        else
-          @order = order
-          @products = @cart.products
-          @sum = @cart.sum
-          return render(:new, status: :unprocessable_entity)
-        end
+    unless user_signed_in?
+      existing_users = User.where(:email => params[:order][:email])
+      if existing_users == []
+        user = create_user
+        sign_in(user)
+      else
+        redirect_to new_user_session_path
+        return
       end
+    end
 
-      payment_ids = payments.map do |payment|
-        payment.id
+    @cart.stores.each do |store|
+      order = create_order(store, current_user || user)
+
+      unless order.save
+        @order = order
+        return render(:new, status: :unprocessable_entity)
       end
-      @cart.clear_product
+    end
 
-      redirect_to checkout_payments_path(:ids => payment_ids)
-    # else
-    #   @user = User.create(
-    #   name: :name,
-    #   email: :email,
-    #   phone: :phone,
-    #   password: "qwerty",
-    #   password_confirmation: "qwerty"
-    #   )
-    #   sign_in @user
-    # end
+    payment_ids = @payments.map do |payment|
+      payment.id
+    end
+
+    @cart.clear_product
+
+    redirect_to checkout_payments_path(:ids => payment_ids)
   end
 
   def edit
@@ -98,5 +71,58 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:name, :last_name, :email, :address, :zip, :country, :status)
+  end
+
+  def create_order_product(product, order)
+    order_product = OrderProduct.new(
+      :product_id => product.id,
+      :order_id => order.id
+    )
+    order_product.product_price = product.price - product.discount
+    order_product.product_count = @cart.count(product.id.to_s)
+    order_product.product_name = product.name
+    order_product.product_image = product.images[0]
+    order_product.save
+  end
+
+  def create_order(store, user)
+    order = Order.new(order_params)
+    order.user_id = user.id
+    order.store_id = store.id
+    order.sum_price = 0
+
+    if order.save
+      @cart.products.where(:store_id => store.id).each do |product|
+        create_order_product(product, order)
+      end
+
+      sum_price = 0
+      order.order_products.each do |product|
+        sum_price += product.product_count * product.product_price
+      end
+
+      order.sum_price = sum_price
+
+      if order.save
+        payment = Payment.new(
+          :order_id => order.id,
+          :user_id => order.user_id
+        )
+        payment.save
+        @payments << payment
+      end
+    end
+
+    return order
+  end
+
+  def create_user
+    user = User.create(
+      name: params[:order][:name],
+      email: params[:order][:email],
+      phone: params[:order][:phone],
+      password: "qwerty",
+      password_confirmation: "qwerty"
+    )
   end
 end
