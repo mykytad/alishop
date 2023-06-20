aff8eb155e944830a62bfa1af6450e731ee8f4f9
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-06-20 12:04:49 +0300

    create category filters

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,10 @@ class ProductsController < ApplicationController
     if params[:search].present?
       @products = @products.where("lower(name) like ?", "%#{params[:search].downcase}%")
     end
+
+    if params[:category_id].present?
+      @products = @products.where("lower(category_id) like ?", "%#{params[:category_id].downcase}%" )
+    end
   end
 
   def show
