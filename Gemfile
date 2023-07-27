40fbe5e59ca707d4802a9f7f75c3db76f71cc092
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-07-27 18:11:06 +0300

    add export  gem

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem "mini_magick"
 gem "avatarly"
 gem 'country_select'
 gem 'faker'
+gem 'make_exportable'
 
 gem 'rubocop-rails', require: false
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   has_many :reviews
 
   mount_uploaders :images, ImageUploader
-  serialize :images, JSON # If you use SQLite, add this line.
+  # serialize :images, JSON # If you use SQLite, add this line.
 
   validates :name, presence: true
   validates :description, presence: true
