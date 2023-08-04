6aba12b7c75d6832cdab3ea76e0cb21966aba92a
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-08-04 16:05:39 +0300

    add json import

--- app/controllers/manager/products_controller.rb
+++ app/controllers/manager/products_controller.rb
@@ -53,7 +53,7 @@ class Manager::ProductsController < ManagerController
       format.json do
         products = store.products
         products_json = products.map do |product|
-          { name: product.name, price: product.price }
+          { name: product.name, price: product.price, discount: product.discount, description: product.description}
         end
         products_json = JSON.generate(products_json)
         # __1__
@@ -77,21 +77,38 @@ class Manager::ProductsController < ManagerController
   def post_import
     file = params[:file]
     file_data = file.read
-    # if file_data ".csv"
-
-    # end
-    file_data = file_data.split("\n")
-    file_data.each do |product_data|
-      product_data = product_data.split(",")
-      Product.create!(
-        name: product_data[0],
-        description: product_data[3],
-        price: product_data[1],
-        discount: product_data[2],
-        category_id: Category.first.id,
-        store_id: @store.id
-      )
+    # binding.pry
+    if params[:file].content_type == 'text/csv'
+      file_data = file_data.split("\n")
+      file_data.each do |product_data|
+        product_data = product_data.split(",")
+        Product.create!(
+          name: product_data[0],
+          description: product_data[3],
+          price: product_data[1],
+          discount: product_data[2],
+          category_id: Category.first.id,
+          store_id: @store.id
+        )
+      end
+    elsif params[:file].content_type == "application/json"
+      file_data = JSON.parse(file_data)
+      binding.pry
+      # file_data = file_data
+      file_data.each do |product_data|
+        Product.create!(
+          name: product_data["name"],
+          description: product_data["description"],
+          price: product_data["price"],
+          discount: product_data["discount"],
+          category_id: Category.first.id,
+          store_id: @store.id
+        )
+      end
+    else
+      redirect_to manager_store_path(@store.id), alert: "import error"
     end
+
     redirect_to manager_store_path(@store.id), notice: "import success"
   end
 
