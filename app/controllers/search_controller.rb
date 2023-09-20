a86fb4b6c9c06a5bdfc52774ee2c9b8279576c3b
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-09-20 14:24:46 +0300

    add spec_helper

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
   def elsearch
     @products = Product.all
+    # where("lower(name) like ?", "%#{params[:query].downcase}%")
+    elastic_query = @products.where("lower(name) like ?", "%#{params[:query].downcase}%")
     @products = Product.search(params[:query])
   end
 end
