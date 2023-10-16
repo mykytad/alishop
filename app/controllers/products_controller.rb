d8a8520399b0d45815c68903660b52736c89c95b
Mykyta Dynnik <mykytadynnik@gmail.com>
2023-10-16 16:00:19 +0300

    fix cop

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
     end
 
     if params[:category_id].present?
-      @products = @products.where(:category_id => params[:category_id]) 
+      @products = @products.where(:category_id => params[:category_id])
       @category = Category.find(params[:category_id])
     end
   end
